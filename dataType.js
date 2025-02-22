ea27202d11031b1a6cf3b9b93cd78472b29634ab
Hajjaj <hajjaj149138@gmail.com>
2025-02-22 11:36:27 +0600

    22/2

---
+++ dataType.js
@@ -0,0 +1,18 @@
+let message = "Hello, World!";  // String
+let age = 30;                   // Number
+let isActive = true;            // Boolean
+let user = null;                // Null
+let address;                    // Undefined (since no value is assigned)
+
+let person = { name: "Alice", age: 25 };  // Object
+let fruits = ["Apple", "Banana", "Orange"];  // Array
+
+// Printing each variable using console.log()
+console.log(message);  // Output: Hello, World!
+console.log(age);      // Output: 30
+console.log(isActive); // Output: true
+console.log(user);     // Output: null
+console.log(address);  // Output: undefined
+
+console.log(person);   // Output: { name: 'Alice', age: 25 }
+console.log(fruits);   // Output: [ 'Apple', 'Banana', 'Orange' ]
---
+++ start.js
@@ -0,0 +1,7 @@
+let name = "Md. Hajjaj Bin Sonosi" ;
+const age = 25 ;
+
+name = "Shawon Khan" ;
+
+console.log("My name is : " + name ) ;
+console.log(age) ;
