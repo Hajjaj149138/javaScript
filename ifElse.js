57a932382256e5fef7cae7f57d5f9c69c5741bb7
Hajjaj <hajjaj149138@gmail.com>
2025-03-20 23:37:47 +0600

    Conditional Statements

---
+++ ifElse.js
@@ -0,0 +1,9 @@
+let age = 30 ;
+
+if (age < 25 ) {
+    console.log("Sorry, You are under age!!!");
+} else if (age == 25) {
+    console.log("You are in the waiting list for exact 25 years of old!!!");
+} else {
+    console.log("Congratulations, You are eligible for this post to apply...");
+}
---
+++ switch.js
@@ -0,0 +1,15 @@
+let day = "Friday" ;
+
+switch(day) {
+    case "Saturday":
+        console.log("First day at office after offday in a week...");
+        break;
+    case "Thursday":
+        console.log("Last day at office before offday in a week...");
+        break;
+    case "Friday":
+        console.log("Offday at office in every week...");
+        break;
+    default:
+        console.log("Mid-working day at the office...")
+}
