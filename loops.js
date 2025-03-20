3c9d5f44e4f795f625fb75dee1dd209fdd5cadfe
Hajjaj <hajjaj149138@gmail.com>
2025-03-21 00:09:29 +0600

    Loops

---
+++ loops.js
@@ -0,0 +1,47 @@
+        //   For loop
+
+// for (let i = 0; i<5; i++) {
+//     console.log(i) ;
+// }
+
+// for (let i = 0 ; i<5 ; i++) {
+//     console.log(i + ".    I love you!!!") ;
+// }
+
+        //    While Loop
+
+
+// let i = 0 ;
+
+// while (i<5) {
+//     console.log(i);
+//     i++;
+// }
+
+        // Do While Loop
+
+// let i = 0;
+// do {
+//   console.log(i);  // prints 0 to 4
+//   i++;
+// } while (i < 5);
+
+// let i = 0 ;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i<5);
+
+            // For of loop
+
+// let fruits = ["Apple", "Banana", "Mango"];
+// for (let fruit of fruits) {
+//     console.log(fruit);
+// }
+
+        // For in Loop
+
+let person = {name : "John" , age : 25 } ;
+for(let key in person) {
+    console.log(key + " : " + person[key]) ;
+}
